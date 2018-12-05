4d868df46e578d3bdc6af1bce98fb4a7f5b90307
David Feng <jiguanglizipao@163.com>
2018-12-05 16:30:14 +0800

    add pullpeers

--- PACoin.py
+++ PACoin.py
@@ -12,32 +12,88 @@ import grpc
 import PACoin_pb2
 import PACoin_pb2_grpc
 
-class P2PDiscoveryServicer(PACoin_pb2_grpc.P2PDiscoveryServicer):
-    def ping(self, request, context):
-        ip = context.peer().split(':')[1]
-        print("receive: ", ip, request.port)
-        return PACoin_pb2.PingReply(ret=PACoin_pb2.SUCCESS)
-
 class PACoin:
+
+    class P2PDiscoveryServicer(PACoin_pb2_grpc.P2PDiscoveryServicer):
+        def __init__(self, pacoin):
+            super().__init__()
+            self.pacoin = pacoin
+
+        def ping(self, request, context):
+            ip = context.peer().split(':')[1]
+            print("receive ping: ", ip, request.port)
+            self.pacoin.init_latency("%s:%d" % (ip, request.port))
+            return PACoin_pb2.PingReply(ret=PACoin_pb2.SUCCESS)
+
+        def pullPeers(self, request, context):
+            ip = context.peer().split(':')[1]
+            print("receive pullPeers: ", ip, request.num)
+            return PACoin_pb2.PullPeersReply(ret=PACoin_pb2.SUCCESS, hosts=self.pacoin.list_peers(request.num))
+
+
     def KeyboardInterruptHandler(self, signum, frame):
         self.to_exit = True
 
-    def __init__(self, server:grpc.Server, bind:str, port:int, first_peer:str, db:sqlite3.Connection):
+    def __init__(self, server:grpc.Server, bind:str, port:int, first_peer:str, db:sqlite3.Connection, timeout:float, peer_num:int):
         self.to_exit = False
         self.server = server
         self.first_peer = first_peer
         self.bind = bind
         self.port = port
         self.db = db
+        self.db_mutex = threading.Lock()
+        self.timeout = timeout
+        self.peer_num = peer_num
         atexit.register(self.cleanup)
 
     def cleanup(self):
         self.db.commit()
         self.db.close()
 
+    def update_latency(self, peer, latency):
+        self.db_mutex.acquire()
+        cursor = self.db.cursor()
+        cursor.execute("UPDATE peers SET latency = ? WHERE host = ?", (latency, peer))
+        if cursor.rowcount == 0:
+            cursor.execute("INSERT INTO peers (host, latency) VALUES (?, ?)", (peer, latency))
+#        cursor.execute("SELECT * FROM peers WHERE host = ?", (peer, ))
+#        print(cursor.fetchone())
+        cursor.close()
+        self.db_mutex.release()
+
+    def init_latency(self, peer):
+        self.db_mutex.acquire()
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM peers WHERE host = ?", (peer, ))
+        if cursor.rowcount == 0:
+            cursor.execute("INSERT INTO peers (host, latency) VALUES (?, ?)", (peer, self.timeout))
+#        print(cursor.fetchone())
+        cursor.close()
+        self.db_mutex.release()
+
+    def list_peers(self, num):
+        self.db_mutex.acquire()
+        cursor = self.db.cursor()
+        cursor.execute("SELECT host FROM peers ORDER BY latency ASC LIMIT ?", (num, ))
+#        print(cursor.fetchone())
+        arr = [r[0] for r in cursor.fetchall()]
+        cursor.close()
+        self.db_mutex.release()
+        return arr
+
+    def shrink_peers(self, num, if_print=False):
+        self.db_mutex.acquire()
+        cursor = self.db.cursor()
+        cursor.execute("DELETE FROM peers WHERE host NOT IN (SELECT host FROM peers ORDER BY latency ASC LIMIT ?)", (num, ))
+        if if_print:
+            cursor.execute("SELECT * FROM peers")
+            print("current peers: ", cursor.fetchall())
+        cursor.close()
+        self.db_mutex.release()
+
     def serve(self):
         PACoin_pb2_grpc.add_P2PDiscoveryServicer_to_server(
-            P2PDiscoveryServicer(), self.server)
+            self.P2PDiscoveryServicer(self), self.server)
         self.server.add_insecure_port("%s:%d" % (self.bind, self.port))
         self.server.start()
 
@@ -47,12 +103,33 @@ class PACoin:
             with grpc.insecure_channel(peer) as channel:
                 stub = PACoin_pb2_grpc.P2PDiscoveryStub(channel)
                 response = stub.ping(
-                    PACoin_pb2.PingRequest(port=self.port), timeout=1)
+                    PACoin_pb2.PingRequest(port=self.port), timeout=self.timeout*1e-3)
                 latency = time.time() - start_time
                 print("ping ", peer, ", latency = %.2f ms" %
                       (latency.real * 1e3))
+                self.update_latency(peer, 1000*latency)
         except Exception as e:
             print(e)
+
+    def pullPeers(self, peer):
+        try:
+            with grpc.insecure_channel(peer) as channel:
+                stub = PACoin_pb2_grpc.P2PDiscoveryStub(channel)
+                response = stub.pullPeers(
+                    PACoin_pb2.PullPeersRequest(num=self.peer_num), timeout=self.timeout*1e-3)
+                print("pullPeers ", peer, ", hosts = ", response.hosts)
+                for host in response.hosts:
+                    self.init_latency(host)
+        except Exception as e:
+            print(e)
+
+    def update_peers(self):
+        self.shrink_peers(self.peer_num)
+        for p in self.list_peers(self.peer_num):
+            self.pullPeers(p)
+        for p in self.list_peers(self.peer_num*(self.peer_num+1)):
+            self.ping(p)
+        self.shrink_peers(self.peer_num, True)
 
     def loop(self, seconds, func, *args):
         func(*args)
@@ -61,9 +138,13 @@ class PACoin:
             timer.start()
 
     def start(self):
+        cursor = self.db.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS peers (host VARCHAR(128) PRIMARY KEY, latency REAL)")
+        cursor.close()
+        self.init_latency(self.first_peer)
         signal.signal(signal.SIGINT, self.KeyboardInterruptHandler)
         self.serve()
-        self.loop(2, self.ping, self.first_peer)
+        self.loop(10, self.update_peers)
         while not self.to_exit:
             time.sleep(1)
 
@@ -72,10 +153,12 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, default=23333, help='Listening port')
     parser.add_argument('--bind', type=str, default="[::]", help='Listening ip')
     parser.add_argument(
-        '--peer', type=str, default="localhost:23333", help='First peer to connect')
+        '--peer', type=str, default="127.0.0.1:23333", help='First peer to connect')
     parser.add_argument('--db', type=str, default="PACoin.db", help='Database file name')
+    parser.add_argument('--timeout', type=float, default=2000.0, help='Threshold for timeout')
+    parser.add_argument('--peer_num', type=int, default=5, help='Maximum numbers of peers')
     args = parser.parse_args()
     logging.basicConfig()
 
-    pacoin = PACoin(server=grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=4)), bind=args.bind, port=args.port, first_peer=args.peer, db=sqlite3.connect(args.db))
+    pacoin = PACoin(server=grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=4)), bind=args.bind, port=args.port, first_peer=args.peer, db=sqlite3.connect(args.db, check_same_thread = False), timeout=args.timeout, peer_num=args.peer_num)
     pacoin.start()
