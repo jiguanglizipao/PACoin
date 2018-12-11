cc01549fe72287fe3b0fd2b9208a66bfb568bdaa
xlthu <xie.lei@foxmail.com>
2018-12-11 19:32:17 +0800

    fix bug

--- PACoin.py
+++ PACoin.py
@@ -396,7 +396,7 @@ class PACoin:
                 with grpc.insecure_channel(peer) as channel:
                     stub = PACoin_pb2_grpc.BlockTransferStub(channel)
                     stub.sendBlocks(
-                        PACoin_pb2.SendBlocksRequest(block=PACoin_pb2.Block(data=blk))
+                        PACoin_pb2.SendBlocksRequest(block=PACoin_pb2.Block(data=utils.bytes2Data(blk)))
                     )
             except Exception as e:
                 mysqlite.delete_peer(self.db, self.db_mutex, peer)
